f5abf31899445ba193a18af133a7f3c00ee30142
AlexAksioma <alexmedqwerty@gmail.com>
2023-03-12 14:19:10 +0200

    add class RegistrationTests

---
+++ src/test/java/ui_tests/AlexMed/RegistrationTests.java
@@ -0,0 +1,14 @@
+package ui_tests.AlexMed;
+
+import org.testng.annotations.Test;
+import ui_tests.TestBase;
+
+import java.lang.annotation.Target;
+
+public class RegistrationTests extends TestBase {
+
+    @Test
+    public void registrationPositiveTest(){
+        System.out.println("reg Positive test");
+    }
+}
